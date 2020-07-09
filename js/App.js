5eb6862d13e53d31b07b3ed785dfe7d13e6ee64f
msitu@ceresimaging.net <60153807+msitu@users.noreply.github.com>
2020-07-09 12:23:42 -0700

    make default props easier to swap for dev

--- js/App.js
+++ js/App.js
@@ -5,14 +5,22 @@ import Rej from './components/Rej.vue'
 
 Vue.config.productionTip = false
 
+let imageryURL
+let referenceURL
+imageryURL = "demo/vnir-imagery-distorted.png"
+referenceURL = "demo/vnir-imagery.png"
+
+// imageryURL = "demo/2019-09-23 1380 Harlan Blocks A Jenoptik.tif"
+// referenceURL = "demo/Harlan Blocks A VNIR.tif"
+
 export default Vue.extend({
   vuetify,
   store,
   render: (h) =>
     h(Rej, {
       props: {
-        imageryURL: "demo/vnir-imagery-distorted.png",
-        referenceURL: "demo/vnir-imagery.png",
+        imageryURL,
+        referenceURL,
       }
     })
 })
